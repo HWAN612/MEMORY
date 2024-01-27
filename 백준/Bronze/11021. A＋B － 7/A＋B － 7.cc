96c9e8289923b3e5707b5a9ce2d84b8dc77b5820
HWAN <152396489+HWAN612@users.noreply.github.com>
2024-01-27 19:27:00 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+	int T;
+	cin >> T;
+	int* A = new int[T];
+	int* B = new int[T];
+
+	for (int i = 0; i < T; i++)
+	{
+		cin >> A[i] >> B[i];
+	}
+
+	for (int i = 0; i < T; i++)
+	{
+		cout << "Case #" << (i+1) << ": " << A[i] + B[i] << endl;
+	}
+
+}
